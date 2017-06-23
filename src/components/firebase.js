3b14f957018efdef18f754ba904d96442c5d092c
Alexandre DE PEMBROKE <Alexandre.DEPEMBROKE@Digital-Grenoble.local>
2017-06-23 14:20:31 +0200

    update

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,12 +1,7 @@
 import Firebase from 'firebase';
 
 const firebaseApp = Firebase.initializeApp({
-  	apiKey: "AIzaSyDkbH7yrVsgZ5eEZPMzx9htZTT6o_o5S7Q",
-    authDomain: "moneyapp-ec342.firebaseapp.com",
-    databaseURL: "https://moneyapp-ec342.firebaseio.com",
-    projectId: "moneyapp-ec342",
-    storageBucket: "moneyapp-ec342.appspot.com",
-    messagingSenderId: "320345538792"
+  	
 });
 
 export const db = firebaseApp.database();
